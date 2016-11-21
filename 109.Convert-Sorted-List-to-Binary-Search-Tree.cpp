a4306068c7628b99cf23718d47bbced64bbc29dc
QiangL-DUT <490308560@qq.com>
2016-11-21 11:02:56 +0800

    Update 109.Convert-Sorted-List-to-Binary-Search-Tree.cpp

--- 109.Convert-Sorted-List-to-Binary-Search-Tree.cpp
+++ 109.Convert-Sorted-List-to-Binary-Search-Tree.cpp
@@ -19,26 +19,26 @@ class Solution {
 public:
     TreeNode* sortedListToBST(ListNode *head) {
         ListNode *temp = head;
-        int n = 0;
+        int len = 0;
         while (temp != NULL) {
             temp = temp->next;
-            ++n;
+            ++len;
         }
-        return sortedListToBST(head, n);
+        return sortedListToBST(head, len);
     }
 private:
-    TreeNode* sortedListToBST(ListNode *head, int n) {
-        if (n <= 0) {
+    TreeNode* sortedListToBST(ListNode *head, int len) {
+        if (len <= 0) {
             return NULL;
         }
         ListNode *mid = head;
         int k;
-        for (k = 0; k < n / 2; ++k) {
+        for (k = 0; k < len / 2; ++k) {
             mid = mid->next;
         }
         TreeNode *root = new TreeNode(mid->val);
         root->left = sortedListToBST(head, k);
-        root->right = sortedListToBST(mid->next, n - k - 1);
+        root->right = sortedListToBST(mid->next, len - k - 1);
         return root;
     }
 };
