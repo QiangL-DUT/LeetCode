378024e2d7a9996dbf1a251333d8546d97cde8a5
QiangL-DUT <490308560@qq.com>
2016-11-23 21:55:09 +0800

    Update 453.Minimum-Moves-to-Equal-Array-Elements.cpp

--- 453.Minimum-Moves-to-Equal-Array-Elements.cpp
+++ 453.Minimum-Moves-to-Equal-Array-Elements.cpp
@@ -6,26 +6,9 @@ public:
         int n = nums.size();
         for (int i = 0; i < n; ++i) {
             min_val = min(min_val, nums[i]);
-        }
-        for (int i = 0; i < n; ++i) {
             result += nums[i];
         }
         result -= n * min_val;
         return result;
     }
 };
-
-/* original
-class Solution {
-public:
-    int minMoves(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        int result = 0;
-        int n = nums.size();
-        for (int i = 1; i < n; ++i) {
-            result += (nums[i-1] - nums[i]) * i;
-        }
-        return result;
-    }
-};
-*/
