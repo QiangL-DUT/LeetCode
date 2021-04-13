6a1afc5e635be431a00f811696b72761b3c89b78
davidqli <davidqli@tencent.com>
2021-04-13 14:46:50 +0800

    calculate auc

--- calculate_auc.py
+++ calculate_auc.py
@@ -17,3 +17,19 @@ def calculate_auc(labels, probs):
             neg_acc += 1
             auc += pos_acc
     return auc / (pos_num * neg_num)
+
+
+def main():
+    data = pd.read_csv("", usecols=['label', 'prob'])
+    labels = data['label'].values
+    probs = data['prob'].values
+    auc = calculate_auc(labels, probs)
+    print("calculate_auc: %s" % auc)
+
+    fpr, tpr, _ = metrics.roc_curve(labels, probs, pos_label=1)
+    sklearn_auc = metrics.auc(fpr, tpr)
+    print("sklearn_auc: %s" % sklearn_auc)
+
+
+if __name__ == '__main__':
+    main()
